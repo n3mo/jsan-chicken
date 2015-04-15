0181e297a34b89f075d88dd5f62495f21f03837f
nemo <nemo1211@gmail.com>
2015-04-15 15:56:23 -0400

    Version bump reflecting new string quoting

--- json2csv.scm
+++ json2csv.scm
@@ -5,7 +5,7 @@
 
 ;; Author: Nicholas M. Van Horn <vanhorn.nm@gmail.com>
 ;; Keywords: json csv convert conversion cli terminal command line
-;; Version: 1.1.0
+;; Version: 1.2.0
 
 ;; This file is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -32,7 +32,7 @@
 ;;; see the github page for this program at
 ;;; https://github.com/n3mo/dvr 
 
-(define json2csv-version "1.1.0 (2015-03-13)")
+(define json2csv-version "1.2.0 (2015-04-15)")
 
 (require-extension args)
 (require-extension files)
